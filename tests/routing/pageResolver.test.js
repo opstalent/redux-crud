d63e8b124b20f402c2c64d49542e18bbae1016d9
Patryk Grudniewski <patgrudniewski@gmail.com>
2017-10-20 15:05:47 +0200

    Refactor of routeResolver

---
+++ tests/routing/pageResolver.test.js
@@ -0,0 +1,18 @@
+import { expect } from 'chai';
+
+import CreateForm from '../../src/page/CreateForm.js';
+import resolver from '../../src/routing/pageResolver.js';
+
+describe('routing/pageResolver', () => {
+  it('is a function', () => {
+    expect(resolver).to.be.a('function');
+  });
+
+  it('throws an exception when given type is not valid', () => {
+    expect(() => resolver()).to.throw();
+  });
+
+  it('returns `CreateForm` component when given type is `add`', () => {
+    expect(resolver('add')).to.equal(CreateForm);
+  });
+});
